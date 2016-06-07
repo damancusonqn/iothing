dcd3dc6b47de9859c38bbf35e396d04e9b625f7a
Daniel Mancuso <damancuso@gmail.com>
2016-06-08 01:06:47 +0200

    add rboot and support for OTA, not working yet

--- app/application.cpp
+++ app/application.cpp
@@ -4,13 +4,12 @@
 #include <led.h>
 #include <button.h>
 
-// If you want, you can define WiFi settings globally in Eclipse Environment Variables
-#ifndef WIFI_SSID
-	#define WIFI_SSID  "ALICE-WLAN56" // Put you SSID and Password here
-	#define WIFI_PWD "4174363082906394"
-#endif
-
 NtpClient ntpClient("pool.ntp.org", 10*60);
+
+rBootHttpUpdate* otaUpdater = 0;
+
+#define ROM_0_URL "http://192.168.1.140/rom0.bin"
+#define SPIFFS_URL "http://192.168.1.140/rboot.bin"
 
 HttpServer server;
 // static int counter;
@@ -67,8 +66,6 @@ void onTemp(HttpRequest &request, HttpResponse &response)
 	response.sendJsonObject(stream);
 }
 
-
-
 void onFile(HttpRequest &request, HttpResponse &response)
 {
 	String file = request.getPath();
@@ -85,11 +82,69 @@ void onFile(HttpRequest &request, HttpResponse &response)
 }
 
 
+
+
+//____________________
+void OtaUpdate_CallBack(bool result) {
+    if (result == true) {
+        // success
+        uint8 slot;
+        slot = rboot_get_current_rom();
+        if (slot == 0) slot = 1; else slot = 0;
+        // set to boot new rom and then reboot
+        Serial.printf("Firmware updated, rebooting to rom %d...\r\n", slot);
+        rboot_set_current_rom(slot);
+        System.restart();
+    } else {
+        // fail
+        Serial.println("Firmware update failed!");
+    }
+}
+
+void OtaUpdate() {
+
+    uint8 slot;
+    rboot_config bootconf;
+
+    // need a clean object, otherwise if run before and failed will not run again
+    if (otaUpdater) delete otaUpdater;
+    otaUpdater = new rBootHttpUpdate();
+
+    // select rom slot to flash
+    bootconf = rboot_get_config();
+    slot = bootconf.current_rom;
+    if (slot == 0) slot = 1; else slot = 0;
+
+    // flash rom to position indicated in the rBoot config rom table
+    otaUpdater->addItem(bootconf.roms[slot], ROM_0_URL);
+
+    // use user supplied values (defaults for 4mb flash in makefile)
+    if (slot == 0) {
+        otaUpdater->addItem(RBOOT_SPIFFS_0, SPIFFS_URL);
+    } else {
+        otaUpdater->addItem(RBOOT_SPIFFS_1, SPIFFS_URL);
+    }
+
+    // set a callback
+    otaUpdater->setCallback(OtaUpdate_CallBack);
+
+    // start update
+    otaUpdater->start();
+}
+
+void fwUpdate(HttpRequest &request, HttpResponse &response)
+{
+	Serial.println("DGB:Trying fw update...");
+
+    OtaUpdate();
+}
+
 void startWebServer()
 {
 	server.listen(80);
 	server.addPath("/", onIndex);
 	server.addPath("/temp", onTemp);
+	server.addPath("/update", fwUpdate);
 
 	Serial.println("\r\n=== WEB SERVER STARTED ===");
 	Serial.println(WifiStation.getIP());
@@ -126,10 +181,15 @@ void init()
 {
 	// spiffs_mount();
 
+	Serial.println("----------------");
+	Serial.println("I'm the NEW FW3");
+
 	Serial.begin(SERIAL_BAUD_RATE); // 115200 by default
 	Serial.systemDebugOutput(true); // Enable debug output to serial
 
-	WifiStation.config(WIFI_SSID, WIFI_PWD);
+	/*We will use autoconnect (the ESP stores in FLASH the lastest working
+	WiFi credentials) */
+	// WifiStation.config(WIFI_SSID, WIFI_PWD);
 
 	WifiStation.enable(true);
 	WifiAccessPoint.enable(false);
@@ -141,5 +201,7 @@ void init()
 	blueLED.init(LED_PIN);
 	blueLED.blinkSTART(100);
 
-	configButton.init(CONFIG_BUTTON, doSmartConf); //GPIO5
+
+	// configButton.init(CONFIG_BUTTON, OtaUpdate); //GPIO5
+    configButton.init(CONFIG_BUTTON, doSmartConf); //GPIO5
 }
